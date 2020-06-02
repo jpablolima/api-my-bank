1ff8ac164f382186365ba47e1222663ca3308d58
João Pablo <jp.pablolima@gmail.com>
2020-06-02 11:17:07 -0300

    metodo post

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
+app.use(express.json());
 
 app.get('/', function (req, res) {
   res.send("hello world")
+});
+app.post('/account', (req, res) => {
+  let params = req.body;
+  console.log('post account');
 });
 
 app.listen(3000, function () {
